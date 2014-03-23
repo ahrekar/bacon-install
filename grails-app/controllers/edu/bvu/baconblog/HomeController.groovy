c21a1de9159e21bdd7b9c96e779818f564815085
Karl Ahrendsen <karlahr2010@gmail.com>
2014-03-23 11:47:22 -0500

    Fixed an error where the program would throw a null exception if the query was not found.

--- grails-app/controllers/edu/bvu/baconblog/HomeController.groovy
+++ grails-app/controllers/edu/bvu/baconblog/HomeController.groovy
@@ -11,7 +11,10 @@ class HomeController {
 			Post postTitle = Post.findByTitle(params.searchQuery)
 			Post postContent = Post.findByContent(params.searchQuery)
             posts = Post.findAllByUser(user)
-			posts += tag.posts
+			// If ther are tags the match the search query,
+			// add any posts that have that tag
+			if (tag)
+				posts += tag.posts
 
         } else {
             posts = Post.findAll()
