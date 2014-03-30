9e4905c58e44db9e09b5ceeccc461d57c7a1f113
Karl Ahrendsen <karlahr2010@gmail.com>
2014-03-30 11:56:26 -0500

    Search now returns a set of items, no duplicate items are shown in a search. I believe this completes objective 3.

--- grails-app/controllers/edu/bvu/baconblog/HomeController.groovy
+++ grails-app/controllers/edu/bvu/baconblog/HomeController.groovy
@@ -2,7 +2,8 @@ package edu.bvu.baconblog
 
 class HomeController {
     def index() {
-        def posts = [] as Set
+        def postList = [] 
+		def posts = []
 
         if (params.searchQuery) {
 
@@ -10,11 +11,14 @@ class HomeController {
 			Tag tag = Tag.findByName(params.searchQuery)
 			Post postTitle = Post.findByTitle(params.searchQuery)
 			Post postContent = Post.findByContent(params.searchQuery)
-            posts = Post.findAllByUser(user)
+            postList = Post.findAllByUser(user)
 			// If ther are tags the match the search query,
 			// add any posts that have that tag
 			if (tag)
-				posts += tag.posts
+				postList.addAll(tag.posts)
+
+			// Remove any duplicates from the list
+			posts = postList as Set
 
         } else {
             posts = Post.findAll()
