4d10be28acde67cf2d900629258e33aeaea8d0c8
Karl Ahrendsen <karlahr2010@gmail.com>
2014-03-30 13:30:46 -0500

    Fixed search now actually searches through content before it was just matching content that was exactly the same as the search query.

--- grails-app/controllers/edu/bvu/baconblog/HomeController.groovy
+++ grails-app/controllers/edu/bvu/baconblog/HomeController.groovy
@@ -7,13 +7,29 @@ class HomeController {
 
         if (params.searchQuery) {
 
-            User user = User.findByUserId(params.searchQuery)
-			Tag tag = Tag.findByName(params.searchQuery)
-			Post postTitle = Post.findByTitle(params.searchQuery)
-			Post postContent = Post.findByContent(params.searchQuery)
-            postList = Post.findAllByUser(user)
-			// If ther are tags the match the search query,
-			// add any posts that have that tag
+            User user = User.findByUserIdIlike(params.searchQuery)
+			Tag tag = Tag.findByNameIlike(params.searchQuery)
+
+			// Gather all of the posts.
+			def somePosts = []
+			somePosts = Post.findAll()
+
+			// Search through the content of each post for the search
+			// query.
+			for(Post post : somePosts){
+				if(post.getContent().contains(params.searchQuery)){
+					postList.add(post)
+				}
+				else if(post.getTitle().contains(params.searchQuery)){
+					postList.add(post)
+				}
+			}
+
+			// Add the posts that have user names that match the query.
+            postList += Post.findAllByUser(user)
+
+			// If there are tags that match the search query,
+			// add any posts that have that tag.
 			if (tag)
 				postList.addAll(tag.posts)
 
